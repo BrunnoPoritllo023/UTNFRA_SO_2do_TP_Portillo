154d3c68788705828abd6add5614ce6e06c7242d
brunoportillo <brunolportillooficial@gmail.com>
2025-11-11 04:42:50 -0300

    Punto_B.sh

--- RTA_Examen_20251111/Punto_B.sh
+++ RTA_Examen_20251111/Punto_B.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# AltaUsers script
+
 USUARIO_BASE="$1"
 ARCHIVO_LISTA="$2"
 
@@ -8,13 +7,11 @@ if [ -z "$USUARIO_BASE" ] || [ -z "$ARCHIVO_LISTA" ]; then
   exit 1
 fi
 
-# Extraer hash de password del usuario base
 CLAVE_HASH=$(sudo awk -F: -v u="$USUARIO_BASE" ' $1==u {print $2}' /etc/shadow)
 
 while IFS=, read -r GRUPO USUARIO; do
   [ -z "$GRUPO" ] && continue
   [ -z "$USUARIO" ] && continue
   sudo groupadd -f "$GRUPO"
-  # Crear usuario con la misma contraseña (hash)
   sudo useradd -m -g "$GRUPO" -p "$CLAVE_HASH" "$USUARIO" || true
 done < "$ARCHIVO_LISTA"
